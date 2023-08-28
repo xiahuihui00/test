08d52d98bd6607bcee30beb92e80dbc4034a291a
loye <1438387995@qq.com>
2023-08-28 11:25:49 +0800

    数据类型

---
+++ 01_数据类型.py
@@ -0,0 +1,21 @@
+# _*_coding : utf-8 _*_
+# @Time : 2023/6/21 14:52
+# @Author : xhh
+# @File : 01_数据类型
+# @Project : 代码
+# int
+# float
+# boolean
+# string
+# list
+# tuple
+# dict
+age = [20, 21, 22]
+# <class 'list'>
+print(type(age))
+gender = ('男', '女')
+# <class 'tuple'>
+print(type(gender))
+info = {'age': 12, 'sex': '男'}
+# <class 'dict'>
+print(type(info))
